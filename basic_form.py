023b7f2aa28f1eb433399089b5a49b58ff265f52
Leilani Battle <leibatt@gmail.com>
2012-12-04 07:54:25 -0800

    got toy form site working. added a redirect to a new page on successful registration.

--- basic_form.py
+++ basic_form.py
@@ -5,4 +5,3 @@ class RegistrationForm(Form):
     email        = TextField('Email Address', [validators.Length(min=6),validators.Email(message="this doesn't seem like an email")])
     password        = TextField('Password', [validators.Length(min=6, max=35,message="must be 6-35 characters")])
     accept_rules = BooleanField('I accept the site rules', [validators.Required(message="must accept site rules to proceed")])
-
--- form_serv.py
+++ form_serv.py
@@ -1,26 +1,32 @@
-from flask import Flask, session, request, render_template, g, redirect, send_file
-import basic_form as bform
-import random
-import simplejson as json
-#import md5
-import traceback
-import socket
-import sys
-import uuid
-import websocket
-import logging
-from logging.handlers import RotatingFileHandler
-from logging import Formatter
+from flask import Flask,flash, session, request, render_template, g, redirect, send_file, url_for
+import basic_form
 app = Flask(__name__)
 
+app.secret_key = "4"
+
 @app.route('/reg/', methods=["POST", "GET"])
-def get_data2():
-    return render_template('register.html')
+def main():
+    form = basic_form.RegistrationForm()
+    return render_template('register.html',form=form)
 
 @app.route('/login/', methods=["POST"])
-def register(request):
-    form = RegistrationForm(request.POST)
+def register():
+    form = basic_form.RegistrationForm(request.form)
     if form.validate():
         print "received username:",form.username.data
         print "received email:",form.email.data
         print "accept rules:",form.accept_rules.data
+        flash('Thanks for registering')
+        return redirect(url_for('done'))
+    return render_template('register.html', form=form)
+
+@app.route('/done/',methods=["POST","GET"])
+def done():
+    return render_template('done.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    #address = ('', 8080)
+    #http_server = WSGIServer(address, app, handler_class=WebSocketHandler)
+    #http_server.serve_forever()
+    app.run()
